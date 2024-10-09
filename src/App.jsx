5124408fec31ca89ecdc4613e23534a1d7d6dc78
NikCodePro <vermanikhil7838@gmail.com>
2024-10-09 11:27:33 +0530

    react app started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/navbar"
+
+const App = () => {
+  return (
+    <>
+    <div>App</div>
+    <Navbar/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
